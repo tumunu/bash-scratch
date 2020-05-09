34e8d70819f290a483fb0a3624c285626daead3b
TCP <tcp@tumunu.com>
2020-05-09 02:00:30 -0400

    need to work on the arg check logic

--- virtualbox/vboxsf.sh
+++ virtualbox/vboxsf.sh
@@ -4,13 +4,18 @@
 version=alpha
 name=vboxsf.sh
 
+helper() {
+	echo 
+	echo "Usage: $name -f | --fix [remote share name][/path/to/local/directory]"
+	echo "e.g. $name -f temp /tmp"
+	echo
+	exit 1
+}
+
 fix_vboxsf() {
 	# check for arguments
-	if [ "$1" == "" ] || [ $# -gt 1 ]; then
-		echo
-		echo "Usage: $name -f | --fix [remote share name][/path/to/local/directory]"
-		echo "e.g. $name -f temp /tmp"
-		exit 1
+	if [ "$1"];  then
+		helper
 	else
 		sudo mount -t vboxsf -o uid=$UID,gid=$(id -g) $1 $2
 	fi
@@ -21,10 +26,7 @@ case $1 in
 		fix_vboxsf $2 $3
 		;;
 	*)
-		echo
-		echo "Usage: $name -f | --fix [remote share name][/path/to/local/directory]"
-		echo
-		exit 1
+		helper
 		;;
 esac
 	
